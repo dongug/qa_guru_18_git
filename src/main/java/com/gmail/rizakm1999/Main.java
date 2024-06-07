003764f9bb171dd5a02fe9927686cf52eaa652c4
dongug <44321033+dongug@users.noreply.github.com>
2024-06-07 17:44:01 +0500

    Update Main.java

--- src/main/java/com/gmail/rizakm1999/Main.java
+++ src/main/java/com/gmail/rizakm1999/Main.java
@@ -2,7 +2,7 @@ package com.gmail.rizakm1999;
 
 public class Main {
     public static void main(String[] args) {
-        private static String msg = "Hello GURU!!!!!!!!");
+        private static String msg = "Hello GURU!!!!!!!!";
         System.out.println(msg);
     }
 }
