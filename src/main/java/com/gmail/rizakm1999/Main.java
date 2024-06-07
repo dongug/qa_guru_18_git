e690a3e9302f1c7e5ec961ec7151f5a662379549
Akmal Rizaev <testing300bucks@gmail.com>
2024-06-07 17:32:29 +0500

    Added getMessage() method

--- src/main/java/com/gmail/rizakm1999/Main.java
+++ src/main/java/com/gmail/rizakm1999/Main.java
@@ -2,6 +2,10 @@ package com.gmail.rizakm1999;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello GURU!");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello qa.guru";
     }
 }
